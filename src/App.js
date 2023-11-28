7e02ecdf62313f90a586c56e3540fc1f89af29a2
WdSevilla <45079501+WdSevilla@users.noreply.github.com>
2023-11-27 21:51:49 -0600

    first edit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+export default function App() {
+  const [imageUrl, setImageUrl] = useState('');
 
-export default App;
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    // Lógica para analizar la imagen
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para generar la imagen
+  };
+
+  return (
+    <div>
+      <h1>Título</h1>
+      <input type="text" value={imageUrl} onChange={handleImageUrlChange} placeholder="URL de la imagen" />
+      <button onClick={handleImageAnalysis}>Analizar imagen</button>
+      <button onClick={handleImageGeneration}>Generar imagen</button>
+    </div>
+  );
+}
+import React, { useState } from 'react';
+
+
